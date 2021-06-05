22f95f2711b2c71bd34c101ace3ffe7e4b83b174
prateekarora0819 <53963173+prateekarora0819@users.noreply.github.com>
2021-06-05 17:38:26 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+export default App;
